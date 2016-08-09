c935810fb0464d86dda72dc88329667701344aa5
Elizabeth <eharper@excelmicro.com>
2016-08-09 15:59:55 -0400

    Added license.  Using Apache-2.0

--- src/main/java/io/vulpine/util/cli/MultiApplication.java
+++ src/main/java/io/vulpine/util/cli/MultiApplication.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Elizabeth Harper
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.vulpine.util.cli;
 
 import io.vulpine.util.cli.def.CliArgumentInterface;
@@ -13,29 +28,33 @@ public class MultiApplication extends CliApplication
 
   protected final Map < String, CliModeInterface > modes;
 
-  public MultiApplication ( final String[] args )
+  public MultiApplication( final String[] args )
   {
     super(args);
     modes = new HashMap < String, CliModeInterface >();
   }
 
-  public MultiApplication addAppMode ( final CliModeInterface... c )
+  public MultiApplication addAppMode( final CliModeInterface... c )
   {
-    for ( final CliModeInterface m : c ) { modes.put(m.getName(), m); }
+    for ( final CliModeInterface m : c ) {
+      modes.put(m.getName(), m);
+    }
 
     return this;
   }
 
   @Override
-  public MultiApplication addParameter ( final CliParameterInterface... def )
+  public MultiApplication addParameter( final CliParameterInterface... def )
   {
-    for ( final CliParameterInterface p : def ) { parameters.offer(p); }
+    for ( final CliParameterInterface p : def ) {
+      parameters.offer(p);
+    }
 
     return this;
   }
 
   @Override
-  public void run ()
+  public void run()
   {
     final CliModeInterface                                  mode;
     final Queue < String >                                  params;
@@ -46,7 +65,7 @@ public class MultiApplication extends CliApplication
 
     mode = modes.get(parser.getCliMode());
 
-    if ( mode == null ) {
+    if (mode == null) {
       if (parser.hasFlag("help")) {
         for ( final String l : getHelpText() ) {
           System.out.println(l);
@@ -86,21 +105,23 @@ public class MultiApplication extends CliApplication
     }
 
     for ( final CliArgumentInterface a : arguments.getArguments() ) {
-      if ( a.isRequired() && !a.wasUsed() ) {
+      if (a.isRequired() && !a.wasUsed()) {
         System.out.println(String.format("Argument %s|%s is required.", a.getKey(), a.getName()));
         System.exit(1);
       }
     }
 
     for ( final CliArgumentInterface a : mode.getArgumentSet().getArguments() ) {
-      if ( a.isRequired() && !a.wasUsed() ) {
+      if (a.isRequired() && !a.wasUsed()) {
         System.out.println(String.format("Argument %s|%s is required.", a.getKey(), a.getName()));
         System.exit(1);
       }
     }
 
-    if ( helpFlag.wasUsed() ) {
-      for ( final String l : getHelpText() ) { System.out.println(l); }
+    if (helpFlag.wasUsed()) {
+      for ( final String l : getHelpText() ) {
+        System.out.println(l);
+      }
     } else {
       mode.run(parser);
     }
@@ -108,7 +129,7 @@ public class MultiApplication extends CliApplication
   }
 
   @Override
-  public String[] getHelpText ()
+  public String[] getHelpText()
   {
     final Queue < String > lines = new LinkedList < String >();
     final String[]         out;
@@ -121,43 +142,45 @@ public class MultiApplication extends CliApplication
     }
 
     out = new String[lines.size()];
-    for ( int i = 0; i < out.length; i++ ) { out[i] = lines.poll(); }
+    for ( int i = 0; i < out.length; i++ ) {
+      out[i] = lines.poll();
+    }
 
     return out;
   }
 
-  private void testArg ( final String e, final CliModeInterface mode )
+  private void testArg( final String e, final CliModeInterface mode )
   {
     final CliArgumentInterface a = arguments.getArgument(e);
     testFlag(null == a ? mode.getArgumentSet().getArgument(e) : a, e);
   }
 
-  private void testArg ( final char e, final CliModeInterface mode )
+  private void testArg( final char e, final CliModeInterface mode )
   {
     final CliArgumentInterface a = arguments.getArgument(e);
     testFlag(null == a ? mode.getArgumentSet().getArgument(e) : a, e);
   }
 
-  private void testArg ( final String name, final List < String > values, final CliModeInterface mode )
+  private void testArg( final String name, final List < String > values, final CliModeInterface mode )
   {
     final CliArgumentInterface a = arguments.getArgument(name);
     nullCheckArg(null == a ? mode.getArgumentSet().getArgument(name) : a, name, values);
   }
 
-  private void testArg ( final char key, final List < String > values, final CliModeInterface mode )
+  private void testArg( final char key, final List < String > values, final CliModeInterface mode )
   {
     final CliArgumentInterface a = arguments.getArgument(key);
     nullCheckArg(null == a ? mode.getArgumentSet().getArgument(key) : a, key, values);
   }
 
-  private void testFlag ( final CliArgumentInterface a, final Object e )
+  private void testFlag( final CliArgumentInterface a, final Object e )
   {
-    if ( null == a ) {
+    if (null == a) {
       System.out.println("Unrecognized flag " + e);
       System.exit(1);
     }
 
-    if ( a.getParameter() != null && a.getParameter().isRequired() ) {
+    if (a.getParameter() != null && a.getParameter().isRequired()) {
       System.out.println(String.format("Argument --%s requires a value.", e));
       System.exit(1);
     }
@@ -165,16 +188,16 @@ public class MultiApplication extends CliApplication
     a.use();
   }
 
-  private void nullCheckArg ( final CliArgumentInterface a, final Object i, final List < String > v )
+  private void nullCheckArg( final CliArgumentInterface a, final Object i, final List < String > v )
   {
-    if ( null == a ) {
+    if (null == a) {
       System.out.println("Unrecognized Argument " + i);
       System.exit(1);
     }
     insertValues(a, v);
   }
 
-  private void insertValues ( final CliArgumentInterface a, final List < String > values )
+  private void insertValues( final CliArgumentInterface a, final List < String > values )
   {
     for ( final String s : values ) a.getParameter().parseValue(s);
     a.use();
